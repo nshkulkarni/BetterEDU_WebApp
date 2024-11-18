4dca27e6b18d733ff11fee867afb150ed990d4e0
Nisha Kulkarni <nishakulkarni@Nishas-MacBook-Pro.local>
2024-11-18 13:10:25 -0700

    added .env

--- server/db.js
+++ server/db.js
@@ -1,28 +1,14 @@
 const { Pool } = require('pg');
 
-<<<<<<< HEAD
 const pool = new Pool({
-=======
-const pool = new Pool ({
->>>>>>> cdd5fcc49dd360313385948ff94d155c3bffc4bf
-    user: "nisha_user",
-    password: "123PasswordNisha#!",
-    host: "postgres-1.ctosago6qug9.us-west-1.rds.amazonaws.com",
-    port: "5432",
-    database: "postgres",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     ssl: {
-<<<<<<< HEAD
-        rejectUnauthorized: false, // Use true in production with proper certificates
+        rejectUnauthorized: process.env.DB_SSL === 'true', // Convert string to boolean
     },
 });
 
 module.exports = pool;
-=======
-        rejectUnauthorized: false // Optional: Disable SSL certificate validation (only for testing)
-      }
-});
-
-module.exports = pool; 
-
-
->>>>>>> cdd5fcc49dd360313385948ff94d155c3bffc4bf
