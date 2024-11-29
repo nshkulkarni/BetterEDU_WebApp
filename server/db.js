efdf6e428c18447036bcd5b526299c78d0ecf22a
Girps <jes20221168@gmail.com>
2024-11-29 15:38:48 -0700

    added video streaming component

--- server/db.js
+++ server/db.js
@@ -1,23 +1,15 @@
 const { Pool } = require('pg');
 
-<<<<<<< HEAD
+
 const pool = new Pool({
-=======
-const pool = new Pool ({
->>>>>>> cdd5fcc49dd360313385948ff94d155c3bffc4bf
+
+
     user: "nisha_user",
     password: "123PasswordNisha#!",
     host: "postgres-1.ctosago6qug9.us-west-1.rds.amazonaws.com",
     port: "5432",
     database: "postgres",
     ssl: {
-<<<<<<< HEAD
-        rejectUnauthorized: false, // Use true in production with proper certificates
-    },
-});
-
-module.exports = pool;
-=======
         rejectUnauthorized: false // Optional: Disable SSL certificate validation (only for testing)
       }
 });
@@ -25,4 +17,3 @@ module.exports = pool;
 module.exports = pool; 
 
 
->>>>>>> cdd5fcc49dd360313385948ff94d155c3bffc4bf
